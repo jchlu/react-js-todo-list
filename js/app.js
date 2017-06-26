ce9edd937733185b362dc369f579eb2aa9438952
Johnny Choudhury-Lucas <jchlu+git@me.com>
2017-06-26 21:51:09 +0100

    Start output

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-// You can do this.
+var TodoList = React.createClass({
+    render: function(){
+        return (
+            <div className="todo-list">
+                Todo list
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <TodoList/>, document.getElementById('todo')
+);
